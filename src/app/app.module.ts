abdce27b967280ccb9161e34e02642fdc8f363a6
Muhammad Ali <muhammadalich@Muhammads-MacBook-Pro.local>
2018-12-02 12:50:29 -0600

    Added all component needed for the GPS Tracking Application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ReportComponent } from './report/report.component';
+import { SettingComponent } from './setting/setting.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutusComponent,
+    DashboardComponent,
+    LoginComponent,
+    FooterComponent,
+    HeaderComponent,
+    ReportComponent,
+    SettingComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
